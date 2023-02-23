395ffbf3298b4b6c076ef351e9951c394fd5b931
Alba <alba@gmail.com>
2023-02-23 14:47:46 +0100

    add developers and show methods to controller
    add testing controller methods

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DeveloperController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/developer', function (Request $request) {
+    return $request->developer();
 });
+
+Route::get('developers', [DeveloperController::class, 'developers']);
+Route::get('show/{id}', [DeveloperController::class, 'show']);
